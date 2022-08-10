fae72f0adf8b6866ffd15925be66b49d210d45c8
Josh <70242103+Jyeager89@users.noreply.github.com>
2022-08-10 12:08:48 -0400

    update

--- 8-kyu/Sum Mixed Array.js
+++ 8-kyu/Sum Mixed Array.js
@@ -1,11 +1,10 @@
-//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-//Return your answer as a number.
+// //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// //Return your answer as a number.
 
-function sumMix(x){
+ function sumMix(x){
     const initialValue = 0;
-    const sumWithInitial = x.reduce(
-      (previousValue, currentValue) => Number(previousValue) + Number(currentValue),
+  const sumWithInitial = x.reduce(       (previousValue, currentValue) => Number(previousValue) + Number(currentValue),
       initialValue
-    );
+     );
       return sumWithInitial
     }
